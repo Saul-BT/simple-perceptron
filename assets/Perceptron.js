5fe7aacf3fdbf8942d722ace1db64721108e9442
Saul Blanco Tejero <alu15067@iesgalileo.es>
2020-03-03 00:34:38 +0100

    Adding Perceptron logic

--- assets/Perceptron.js
+++ assets/Perceptron.js
@@ -1,3 +1,26 @@
 export default class Perceptron {
+  constructor() {
+    this.weights = new Array(2)
 
+    // Assing random weights
+    for (let i = 0; i < this.weights.length; i++) {
+      this.weights[i] = Math.floor(Math.random() * 3) - 1
+    }
+  }
+
+  // Step 1 - SUM
+  guess(inputs) {
+    let sum = 0
+
+    this.weights.forEach((weight, index) => {
+      sum += inputs[index] * weight
+    });
+
+    return this.sign(sum)
+  }
+
+  // Step 2 - ACTIVATION FUNCTION
+  sign(num) {
+    return num >= 0 ? 1 : -1
+  }
 }
